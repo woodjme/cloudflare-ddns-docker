ad0cc7cce94dab31ee995b0c42d5da2dfe69d10b
Jamie Wood <jamie.wood@eloquent-technologies.com>
2017-04-19 11:36:29 +0100

    Add update functionality

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const publicIp = require('public-ip');
- 
+const publicIp = require('public-ip'),
+    CloudFlareAPI = require('cloudflare4'),
+    api = new CloudFlareAPI({
+        email: 'process.env.EMAIL',
+        key: 'process.env.APIKEY'
+    });
+
 publicIp.v4().then(ip => {
-    console.log(ip);
+    api.zoneGetAll({
+        name: 'process.env.ZONE'
+    }).then(function (zone) {
+        api.zoneDNSRecordGetAll(zone[0].id, {
+            name: 'process.env.RECORD'
+        }).then(function (record) {
+            api.zoneDNSRecordUpdate(zone[0].id, record[0].id, {
+                type: 'A',
+                content: ip
+            }).then(function (result) {
+                console.log(result);
+            })
+        })
+    });
 });
