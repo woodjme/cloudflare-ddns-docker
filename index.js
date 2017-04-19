3f3dd72a050a0c4cd201cb70a33a8380caa99a61
Jamie Wood <jamie.wood@eloquent-technologies.com>
2017-04-19 11:56:08 +0100

    Add env vars and interval

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const publicIp = require('public-ip'),
     CloudFlareAPI = require('cloudflare4'),
     api = new CloudFlareAPI({
-        email: 'process.env.EMAIL',
-        key: 'process.env.APIKEY'
-    });
+        email: process.env.EMAIL,
+        key: process.env.API_KEY
+    }),
+    minutes = process.env.UPDATE_TIME,
+    the_interval = minutes * 60 * 1000;
 
-publicIp.v4().then(ip => {
-    api.zoneGetAll({
-        name: 'process.env.ZONE'
-    }).then(function (zone) {
-        api.zoneDNSRecordGetAll(zone[0].id, {
-            name: 'process.env.RECORD'
-        }).then(function (record) {
-            api.zoneDNSRecordUpdate(zone[0].id, record[0].id, {
-                type: 'A',
-                content: ip
-            }).then(function (result) {
-                console.log(result);
+setInterval(function () {
+    publicIp.v4().then(ip => {
+        api.zoneGetAll({
+            name: process.env.ZONE
+        }).then(function (zone) {
+            api.zoneDNSRecordGetAll(zone[0].id, {
+                name: process.env.RECORD
+            }).then(function (record) {
+                api.zoneDNSRecordUpdate(zone[0].id, record[0].id, {
+                    type: 'A',
+                    content: ip
+                }).then(function (result) {
+                    console.log(result);
+                })
             })
-        })
+        });
     });
-});
+}, the_interval);
